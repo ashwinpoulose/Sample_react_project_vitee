b677217321f4931e11efca2015114881edb4f19b
Ashwin.Poulose <Ashwin.Poulose@ASHWINPOUL-LT>
2023-05-25 13:38:52 +0530

    Done

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Message from "./Component/Message";
+
+
+function App() {
+  
+
+  return <Message></Message>
+    
+}
+
+export default App
---
+++ src/Component/Message.tsx
@@ -0,0 +1,14 @@
+function Message() {
+    const name="Ashwin";
+
+    if(name)
+    return (
+        <div>
+          
+            <h1>Hello {name}</h1>
+            <h2>I am dabba SMITH CD</h2>
+        </div>
+    )
+}
+
+export default Message
